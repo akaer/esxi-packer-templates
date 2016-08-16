982aab300e68ee37d7c27716911ecf9c0548a088
André Raabe <akaer@users.noreply.github.com>
2016-08-16 13:15:58 +0200

    Add some more settings

--- scripts/esxi-settings.sh
+++ scripts/esxi-settings.sh
@@ -1,2 +1,4 @@
 esxcli network firewall ruleset set -e true -r httpClient
+esxcli network firewall ruleset set -e true -r ntpClient
 esxcli system settings advanced set -o /Net/GuestIPHack -i 1
+esxcli system settings advanced set -o /UserVars/HostClient.CEIPOptIn -i 2
