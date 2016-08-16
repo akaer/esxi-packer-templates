431aea784700cbcb7eb5d952f8efefea4aefc2f3
André Raabe <akaer@users.noreply.github.com>
2016-08-16 16:14:12 +0200

    Remove setting as it doesn't exist at this time

--- scripts/esxi-settings.sh
+++ scripts/esxi-settings.sh
@@ -1,4 +1,3 @@
 esxcli network firewall ruleset set -e true -r httpClient
 esxcli network firewall ruleset set -e true -r ntpClient
 esxcli system settings advanced set -o /Net/GuestIPHack -i 1
-esxcli system settings advanced set -o /UserVars/HostClient.CEIPOptIn -i 2
