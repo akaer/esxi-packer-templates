8f84dbcf747ce4fd663b833d50ca020cfa8b71c8
André Raabe <andre.raabe@gmail.com>
2016-10-18 09:48:08 +0200

    Apply latest esxi patchset

--- scripts/esxi-upgrade.sh
+++ scripts/esxi-upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-esxcli software profile update -p ESXi-6.0.0-20160804001-standard -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+esxcli software profile update -p ESXi-6.0.0-20161004001-standard -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
 reboot
