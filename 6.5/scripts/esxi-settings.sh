5dbdebdb30168e2b552590a23bc03ec718f8a77f
André Raabe <andre.raabe@gmail.com>
2017-06-03 21:02:39 +0200

    Verify checksums and add NTP and additional network config

--- 6.5/scripts/esxi-settings.sh
+++ 6.5/scripts/esxi-settings.sh
@@ -1,3 +1,9 @@
-esxcli network firewall ruleset set -e true -r httpClient
-esxcli network firewall ruleset set -e true -r ntpClient
+esxcli network firewall ruleset set --enabled=true --ruleset-id=httpClient
+esxcli network firewall ruleset set --enabled=true --ruleset-id=ntpClient
 esxcli system settings advanced set -o /Net/GuestIPHack -i 1
+
+echo "server ptbtime1.ptb.de" >>/etc/ntp.conf
+chkconfig --add ntpd
+
+esxcfg-advcfg --set 0 /Mem/ShareForceSalting
+
--- 6.5/scripts/esxi-upgrade_ESXi650-201703001_Build_5146846.sh
+++ 6.5/scripts/esxi-upgrade_ESXi650-201703001_Build_5146846.sh
@@ -4,10 +4,18 @@ set -ex
 
 #esxcli software profile update -p ESXi-6.5.0-20170304001-standard -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
 
-if [[ -e "/vmfs/volumes/datastore1/ESXi650-201703001.zip" ]]; then
-    esxcli software profile update \
-        -d /vmfs/volumes/datastore1/ESXi650-201703001.zip \
-        -p ESXi-6.5.0-20170304001-standard
-    rm -f /vmfs/volumes/datastore1/ESXi650-201703001.zip
-    reboot
+UPDATEFILE="/vmfs/volumes/datastore1/ESXi650-201703001.zip"
+UPDATEPROFILE="ESXi-6.5.0-20170304001-standard"
+
+if [[ -e "${UPDATEFILE}" ]]; then
+    cd /vmfs/volumes/datastore1/
+    if sha1sum -c "${UPDATEFILE}".sha1 ; then
+        esxcli software profile update \
+         -d "${UPDATEFILE}" \
+         -p "${UPDATEPROFILE}"
+        rm -f "${UPDATEFILE}"
+        reboot
+    else
+        echo "Invalid checksum for ${UPDATEFILE}. Update is skipped"
+    fi
 fi
