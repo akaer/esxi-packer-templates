ea378fa3eed16c5d806eac5aa52028fd48029901
André Raabe <andre.raabe@gmail.com>
2017-06-04 18:06:48 +0200

    Add firewall rule to enabled vnc communication on tcp port 5900 for packer.io

---
+++ 6.5/scripts/allow_vnc_firewall.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+FIREWALL_CONFIG=packervnc.xml
+
+cat > /tmp/${FIREWALL_CONFIG} << __CUSTOM_ESXI_FIREWALL_RULE__
+<ConfigRoot>
+  <service>
+    <id>packervnc</id>
+    <rule id='0000'>
+      <direction>inbound</direction>
+      <protocol>tcp</protocol>
+      <porttype>dst</porttype>
+      <port>5900</port>
+    </rule>
+    <rule id='0001'>
+      <direction>outbound</direction>
+      <protocol>tcp</protocol>
+      <porttype>dst</porttype>
+      <port>5900</port>
+    </rule>
+    <enabled>true</enabled>
+    <required>false</required>
+  </service>
+</ConfigRoot>
+__CUSTOM_ESXI_FIREWALL_RULE__
+
+# Copy the new firewall rule to correct directory
+cp /tmp/${FIREWALL_CONFIG} /etc/vmware/firewall/
+
+# Delete the tmp firewall rule
+rm -f /tmp/${FIREWALL_CONFIG}
+
+# Reload the ESXi firewall for the changes to go into effect
+esxcli network firewall refresh
+
